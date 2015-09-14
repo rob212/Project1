fa2472531f5bd3000352791609b5f3c6aab80c53
rob212 <robertmcbryde83@gmail.com>
2015-09-14 22:18:53 +0100

    Addition on UIActivityViewController to share the image.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -31,8 +31,9 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "shareTapped")
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -43,6 +44,11 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        presentViewController(vc, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
